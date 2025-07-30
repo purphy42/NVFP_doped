2cd16b1bc9a113225f6db0ffa6f62a48c95231c4
purphy42 <burov.as@phystech.edu>
2025-07-30 22:13:33 +0300

    auto commit

--- doping.py
+++ doping.py
@@ -14,7 +14,13 @@ import copy
 import os, re
 
 
-if 0:
+
+# st_int.nn(i=27, n=10, only=[23])
+
+# close: 3.57 AA
+# dist: 5.68 AA
+
+if 1:
     # BULK STRUCTURES
     st_int = smart_structure_read("structures/POSCAR_NaVPO4F_su_s10_1u_100_end")
 
@@ -23,20 +29,20 @@ if 0:
     add('bulk.inter', '9_bulk_eos', 1, input_st = st_int, it_folder = 'bulk', calc_method = 'uniform_scale', 
     				ngkpt=[1,1,1], n_scale_images=10, scale_region = (-5, 5), cluster = 'razor128', up='up2', run=2) 
 
-    add('bulk.deinter', '9_bulk_eos', 1, input_st = st_int, it_folder = 'bulk', calc_method = 'uniform_scale', 
+    add('bulk.deinter', '9_bulk_eos', 1, input_st = st_deint, it_folder = 'bulk', calc_method = 'uniform_scale', 
     				ngkpt=[1,1,1], n_scale_images=10, scale_region = (-5, 5), cluster = 'razor128', up='up2', run=2) 
 
 
     st_al_close = st_int.copy()
-    st_al_close = st_al_close.replace_atoms([30, 2], "Al")
+    st_al_close = st_al_close.replace_atoms([27, 28], "Al")
 
-    add('bulk.al.close', '9_bulk_eos', 1, input_st = st_int, it_folder = 'bulk', calc_method = 'uniform_scale', 
+    add('bulk.al.close', '9_bulk_eos', 1, input_st = st_al_close, it_folder = 'bulk', calc_method = 'uniform_scale', 
                 ngkpt=[1,1,1], n_scale_images=10, scale_region = (-5, 5), cluster = 'razor128', up='up2', run=2) 
 
     st_al_dist = st_int.copy()
-    st_al_dist = st_al_dist.replace_atoms([30, 21], "Al")
+    st_al_dist = st_al_dist.replace_atoms([27, 30], "Al")
     
-    add('bulk.al.dist', '9_bulk_eos', 1, input_st = st_int, it_folder = 'bulk', calc_method = 'uniform_scale', 
+    add('bulk.al.dist', '9_bulk_eos', 1, input_st = st_al_dist, it_folder = 'bulk', calc_method = 'uniform_scale', 
                 ngkpt=[1,1,1], n_scale_images=10, scale_region = (-5, 5), cluster = 'razor128', up='up2', run=2) 
  
     st_al_deint = st_al_dist.copy()
@@ -45,8 +51,7 @@ if 0:
     els_del = random.sample(els_na, int(0.9*len(els_na)))
     st_al_deint = st_al_deint.remove_atoms(els_del)
 
-    add('bulk.al.some_na', '9_bulk_eos', 1, input_st = st_int, it_folder = 'bulk', calc_method = 'uniform_scale', 
-    			ngkpt=[1,1,1], n_scale_images=10, scale_region = (-5, 5), cluster = 'razor128', up='up2', run=2) 
+    add('bulk.al.some_na', '9_bulk_eos', 1, input_st = st_al_deint, it_folder = 'bulk', calc_method = 'uniform_scale', ngkpt=[1,1,1], n_scale_images=10, scale_region = (-5, 5), cluster = 'razor128', up='up2', run=2) 
 
     st_some_na = st_int.copy()
     els = st_some_na.get_elements()
@@ -54,7 +59,7 @@ if 0:
     els_del = random.sample(els_na, int(0.9*len(els_na)))
     st_some_na = st_some_na.remove_atoms(els_del)
     
-    add('bulk.some_na', '9_bulk_eos', 1, input_st = st_int, it_folder = 'bulk', calc_method = 'uniform_scale', 
+    add('bulk.some_na', '9_bulk_eos', 1, input_st = st_some_na, it_folder = 'bulk', calc_method = 'uniform_scale', 
             ngkpt=[1,1,1], n_scale_images=10, scale_region = (-5, 5), cluster = 'razor128', up='up2', run=2) 
 
     
@@ -66,11 +71,11 @@ if 0:
 
     # st_al_deint.printme()
 
-    add('bulk.al.some_na_dead', '9_bulk_eos', 1, input_st = st_int, it_folder = 'bulk', ngkpt=[1,1,1], 
+    add('bulk.al.some_na_dead', '9_bulk_eos', 1, input_st = st_al_deint, it_folder = 'bulk', ngkpt=[1,1,1], 
         calc_method = 'uniform_scale', n_scale_images=10, scale_region = (-5, 5), cluster = 'razor128', up='up2', run=2) 
     
 
-if 1:
+if 0:
     # res_loop('bulk.inter.su', '9_bulk_eos', list(range(1,11)) + [100], show = 'fit', analys_type = 'fit_a', cluster = 'razor128')
 
     # res_loop('bulk.deinter.su', '9_bulk_eos', list(range(1,11)) + [100], show = 'fit', analys_type = 'fit_a', cluster = 'razor128')
@@ -101,6 +106,26 @@ if 1:
     add('bulk.some_na', '9_bulk_md_eq', 1, input_st = st_bulk_soma_na, it_folder = 'MD', up="up2", run = 2, cluster = 'razor128') 
     add('bulk.al.some_na_dead', '9_bulk_md_eq', 1, input_st = st_bulk_some_na_dead, it_folder = 'MD', up="up2", run = 2, cluster = 'razor128') 
     
+    
+# db['bulk.al.close.su', '9_bulk_eos', 100].copy().end.printme()
+    
+# db['bulk.al.close.su', '9_bulk_eos', 100].copy().end.jmol()
 
     
+# db['bulk.al.close.su', '9_bulk_eos', 100].copy().res()
+# db['bulk.al.dist.su', '9_bulk_eos', 100].copy().res()
+
+# db['bulk.deinter.su', '9_bulk_eos', 100].copy().end.jmol()
+
+
+if 0:
+    
+    db['bulk.inter.su', '9_bulk_eos', 100].copy().res()
+    db['bulk.deinter.su', '9_bulk_eos', 100].copy().res()
+    db['bulk.al.close.su', '9_bulk_eos', 100].copy().res()
+    db['bulk.al.dist.su', '9_bulk_eos', 100].copy().res()
+    db['bulk.al.some_na.su', '9_bulk_eos', 100].copy().res()
+    db['bulk.some_na.su', '9_bulk_eos', 100].copy().res()
+    db['bulk.al.some_na_dead.su', '9_bulk_eos', 100].copy().res()
+ 
     
